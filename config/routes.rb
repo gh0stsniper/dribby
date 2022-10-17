a08d57bcc1c195eb24f3ebab4d3fb078d02c4dae
suryapoojary0 <suryapoojaryx@protonmail.com>
2022-10-17 17:50:00 +0530

    changed root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :shots
   devise_for :users, controller: { registrations: 'registrations'}
-  root'home#index'
+
+  root 'shots#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")

ae140ccf733911e41917e981e2001b2d7130e15d
suryapoojary0 <suryapoojaryx@protonmail.com>
2022-10-17 19:51:45 +0530

    added add_user_to_shots migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_17_121317) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_17_141947) do
   create_table "shots", charset: "utf8mb4", force: :cascade do |t|
     t.string "title"
     t.text "description"
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "user_shot"
   end
 
   create_table "users", charset: "utf8mb4", force: :cascade do |t|
@@ -27,7 +28,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_17_121317) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
